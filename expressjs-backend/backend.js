41d9dad19bb083c56e0112849523d2e7a14b876c
sean tomer <stomerho@calpoly.edu>
2022-04-06 13:42:37 -0700

    Linked frontend to backend

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cors = require('cors');
 const users = { 
     users_list :
     [
@@ -32,6 +33,7 @@ const users = {
     ]
  }
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/users', (req, res) => {
